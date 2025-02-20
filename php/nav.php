3e622138efbcb415f5c5780956040614a4d10ed6
JacquysBarbosadaSilva <jacquys0801@gmail.com>
2025-02-20 15:11:15 -0300

    first-commit

---
+++ php/nav.php
@@ -0,0 +1,22 @@
+<style>@import url('https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap');
+        </style>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?              family=Gabarito:wght@400900display=swap" rel="stylesheet">
+<div class="alinhamento-nav">
+    <div>
+        <img class="logo-comunica" src="./img/PROJETO GG.png" alt="">
+    </div>
+    
+    <nav>
+        <ul class="nav-links">
+            <li><a href="">Informativo</a></li>
+            <li><a href="">Cardápio</a></li>
+            <li><a href="">Calendário</a></li>
+        </ul>
+    </nav>
+
+    <div class="perfil-user">
+        <img class="icone-user" src="./img/icone-usuario.png" alt="">
+    </div>
+</div>
